d38f931fe5eb4ab2e58b0574d97709917479db26
Irith Katiyar <imkatiya@usc.edu>
2023-10-27 19:45:46 -0700

    nvm i got too excited... this is the demo version

--- StateMachine/utility.py
+++ StateMachine/utility.py
@@ -6,10 +6,7 @@ class Utility(Deed):
         super().__init__(mTileName, mCost, mSet, mRent)
     
     def CalculateRent(self, rollSum: int, player: Player = None) -> int:
-        # TODO: function for number of utility owned
-        # print("IN CALCULATE RENT This is player", player, "This is rollSum", rollSum)
         if self.CountDeedOwned(player) == 1:
-            print(rollSum * 4)
             return rollSum * 4
         elif self.CountDeedOwned(player) == 2:
             return rollSum * 10
