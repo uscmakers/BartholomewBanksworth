5ccf009e2eecb4e0648a9a3f79ca19b9380240ea
Jeremy Pogue <jpogue@usc.edu>
2023-11-02 20:38:46 -0700

    integrated again w electromagnet

--- Embedded/motor.py
+++ Embedded/motor.py
@@ -7,6 +7,15 @@ direction= 22 # Direction (DIR) GPIO Pin
 step = 23 # Step GPIO Pin
 EN_pin = 24 # enable pin (LOW to enable)
 
+#define GPIO pins
+magnet1 = 20 #  GPIO Pin for magnet 1
+magnet2 = 26 #  GPIO pin for magnet 2
+
+# Declare a instance of class pass GPIO pins numbers and the motor type
+# GPIO.setmode(GPIO.OUT)
+GPIO.setup(magnet1,GPIO.OUT) # set enable pin as output for magnet 1
+GPIO.setup(magnet2,GPIO.OUT) # set enable pin as output for magnet 2
+
 # Declare a instance of class pass GPIO pins numbers and the motor type
 mymotortest = RpiMotorLib.A4988Nema(direction, step, (21,21,21), "DRV8825")
 GPIO.setup(EN_pin,GPIO.OUT) # set enable pin as output
@@ -18,3 +27,9 @@ def turnMotor(numSteps: int, clockwise: bool):
                      .0005, # step delay [sec]
                      False, # True = print verbose output 
                      .05) # initial delay [sec]
+    
+def electromagnetOn(num: int):
+    GPIO.output(num, GPIO.HIGH)
+
+def electromagnetOff(num: int):
+    GPIO.output(num, GPIO.LOW)
