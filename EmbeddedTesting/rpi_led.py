2f177721ef53a122954f0a0d52f4ebe38edb8b05
Jeremy Pogue <jpogue@usc.edu>
2024-04-16 23:06:42 -0700

    test

--- EmbeddedTesting/rpi_led.py
+++ EmbeddedTesting/rpi_led.py
@@ -1,7 +1,7 @@
 import board
 import neopixel
 import time
-pixels = neopixel.NeoPixel(board.D18, 30)
+pixels = neopixel.NeoPixel(board.D23, 30)
 
 for x in range(0, 9):
     pixels[x] = (255, 0, 0)
