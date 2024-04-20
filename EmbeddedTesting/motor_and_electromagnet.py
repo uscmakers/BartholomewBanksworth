2ede830bb7d37028c9cbde6f532152a8e4b4e605
Jeremy Pogue <jpogue@usc.edu>
2024-04-19 17:44:54 -0700

    good

--- EmbeddedTesting/motor_and_electromagnet.py
+++ EmbeddedTesting/motor_and_electromagnet.py
@@ -23,7 +23,7 @@ direction= 26 # Direction (DIR) GPIO Pin
 step = 20 # Step GPIO Pin
 EN_pin = 21 # enable pin (LOW to enable)
 
-magnet = 18
+magnet = 17
 
 # Declare a instance of class pass GPIO pins numbers and the motor type
 mymotortest = RpiMotorLib.A4988Nema(direction, step, (21,21,21), "DRV8825")
