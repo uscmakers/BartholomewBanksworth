3898bf3679f48054610c42047db4acfb422f6e4d
Jeremy Pogue <jpogue@usc.edu>
2024-04-21 15:03:58 -0700

    clean up branch

--- StateMachine/goToJail.py
+++
@@ -1,9 +0,0 @@
-from tile import Tile
-from player import Player
-
-class GoToJail(Tile):
-    def __init__(self, mTileName):
-        super().__init__(mTileName)
-
-    def action(self, mPlayer: Player, rollSum: int):
-        mPlayer.GoToJail()
